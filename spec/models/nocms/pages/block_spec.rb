233d3bd4b0d07f0570172126d1a7c8d382bbe295
David J. Brenes <david.brenes@simplelogica.net>
2014-02-27 17:47:43 +0100

    We test that we can update those fields without FactoryGirl
    Just to make sure we can update_attributes for forms

--- spec/models/nocms/pages/block_spec.rb
+++ spec/models/nocms/pages/block_spec.rb
@@ -39,6 +39,40 @@ describe NoCMS::Pages::Block do
         expect(subject.title).to eq block_title
       end
 
+      it("should save nil fields") do
+        expect(subject.body).to be_blank
+      end
+
+      context "when updating various fields" do
+
+        let(:new_block_title) { "new #{Faker::Lorem.sentence}" }
+        let(:block_body) { Faker::Lorem.paragraph }
+
+        before do
+          subject.update_attributes title: new_block_title, body: block_body
+        end
+
+        it("should save info in layout fields") do
+          expect(subject.title).to eq new_block_title
+          expect(subject.body).to eq block_body
+        end
+
+      end
+
+      context "when updating just one field" do
+
+        let(:new_block_title) { "new #{Faker::Lorem.sentence}" }
+
+        before do
+          subject.update_attribute :title, new_block_title
+        end
+
+        it("should save info in layout field") do
+          expect(subject.title).to eq new_block_title
+        end
+
+      end
+
     end
   end
 end
