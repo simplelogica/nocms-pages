4d6be587886cd6dc44fd10c2eb3a58876c3df30d
David J. Brenes <david.brenes@simplelogica.net>
2014-02-27 15:15:17 +0100

    Implementing access to the layout fields
    Yeah, I know this is a trap. We are not really accesing, just responding, because we are not saving any info yet. But hold on! We're coming!

--- app/models/no_cms/pages/block.rb
+++ app/models/no_cms/pages/block.rb
@@ -7,5 +7,19 @@ module NoCMS::Pages
 
     validates :page, :layout, presence: true
 
+    def layout_config
+      NoCMS::Pages.block_layouts.stringify_keys[layout]
+    end
+
+    def method_missing(m, *args, &block)
+      layout_fields = layout_config[:fields]
+
+      if layout_fields[m]
+        m
+      else
+        super
+      end
+    end
+
   end
 end
--- spec/models/nocms/pages/block_spec.rb
+++ spec/models/nocms/pages/block_spec.rb
@@ -2,4 +2,37 @@ require 'spec_helper'
 
 describe NoCMS::Pages::Page do
   it_behaves_like "model with required attributes", :nocms_block, [:layout, :page]
+
+  context "when blocks have layouts" do
+
+    context "with simple fields" do
+
+      before do
+        NoCMS::Pages.configure do |config|
+          config.block_layouts = {
+            'title-long_text' => {
+              template: 'title-long_text',
+              fields: {
+                title: :string,
+                body: :text
+              }
+            }
+          }
+        end
+      end
+
+      let(:block_with_layout) { build :nocms_block, layout: 'title-long_text' }
+
+      subject { block_with_layout }
+
+      it("should respond to layout fields") do
+        expect{subject.title}.to_not raise_error
+        expect{subject.body}.to_not raise_error
+      end
+
+      it("should not respond to fields from other layouts") do
+        expect{subject.no_title}.to raise_error
+      end
+    end
+  end
 end
