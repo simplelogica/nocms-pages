cfb2fe5b7ac0703738c9240391cfabcbe79b2aa6
David J. Brenes <david.brenes@simplelogica.net>
2014-02-27 11:36:56 +0100

    Bug in page migration
    We didn't actually create relation between page and translations.

--- db/migrate/20140226123742_create_no_cms_pages.rb
+++ db/migrate/20140226123742_create_no_cms_pages.rb
@@ -5,6 +5,8 @@ class CreateNoCmsPages < ActiveRecord::Migration
     end
 
     create_table :no_cms_pages_page_translations do |t|
+      t.belongs_to :no_cms_pages_page, index: true
+      t.string :locale
       t.string :title
       t.string :body
     end
