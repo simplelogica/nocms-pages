38090ef940ee37a2f03f4e73e3aafa0d399cf9e6
David J. Brenes <david.brenes@simplelogica.net>
2014-07-01 17:19:57 +0200

    We use the cache_enabled setting for enabling Rails fragment cache

--- app/helpers/no_cms/pages/pages_helper.rb
+++ app/helpers/no_cms/pages/pages_helper.rb
@@ -1,8 +1,19 @@
 module NoCms
   module Pages
     module PagesHelper
-      def render_block block
-        render "no_cms/pages/blocks/#{block.template}", block: block
+      def render_block block, options = {}
+        options[:cache_enabled] ||= block.cache_enabled
+
+        block_template = "no_cms/pages/blocks/#{block.template}"
+
+        if options[:cache_enabled]
+          Rails.cache.fetch "#{block_template}/#{block.id}" do
+            render block_template, block: block
+          end
+        else
+          render block_template, block: block
+        end
+
       end
     end
   end
