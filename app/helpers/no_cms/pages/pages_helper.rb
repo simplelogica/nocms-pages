1fb7b5331827491586ad1d8e072f50d5aa2b8a27
David J. Brenes <david.brenes@simplelogica.net>
2014-07-02 16:13:37 +0200

    wrong has_key method

--- app/helpers/no_cms/pages/pages_helper.rb
+++ app/helpers/no_cms/pages/pages_helper.rb
@@ -5,7 +5,7 @@ module NoCms
         # If cache is disabled for this block then we disable no matter what the block or the options passed have to say about it. This way, the user in the back has the last word about disabling cache
         options[:cache_enabled] = false unless block.page.cache_enabled
         # If we don't have any option about cache enabled then we ask the block
-        options[:cache_enabled] = block.cache_enabled unless options.has_key :cache_enabled
+        options[:cache_enabled] = block.cache_enabled unless options.has_key? :cache_enabled
 
         block_template = "no_cms/pages/blocks/#{block.template}"
 
