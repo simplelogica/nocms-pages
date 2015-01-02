5a1a66b49d423cd8b8666960d05f5579fba9d5fc
David J. Brenes <davidjbrenes@gmail.com>
2015-01-02 13:10:28 +0100

    Decorator for NoCmsBlocks for default positions when linked to pages

---
+++ app/decorators/no_cms/blocks/block_decorator.rb
@@ -0,0 +1,12 @@
+NoCms::Blocks::Block.class_eval do
+
+  after_create :set_default_position
+
+  has_and_belongs_to_many :pages, class_name: 'NoCms::Pages::Page'
+
+  def set_default_position
+    self.update_attribute :position, ((pages.map{|p| p.blocks.pluck(:position)}).flatten.compact.max || 0) + 1) if self[:position].blank? && self.pages.exists?
+  end
+
+end
+
