616e5b6abebc856d2d76c776c121e79e2d42b4fa
David J. Brenes <davidjbrenes@gmail.com>
2015-01-05 17:54:11 +0100

    Including the NoCms::Pages.cache_enabled control on the helper

--- app/helpers/no_cms/pages/pages_helper.rb
+++ app/helpers/no_cms/pages/pages_helper.rb
@@ -2,8 +2,11 @@ module NoCms
   module Pages
     module PagesHelper
       def render_page_block page, block, options = {}
-        # If cache is disabled for this block then we disable no matter what the block or the options passed have to say about it. This way, the user in the back has the last word about disabling cache
-        options[:cache_enabled] = false unless page.cache_enabled
+        # If cache is disabled for all pages or is disabled for this block
+        # then we disable no matter what the block or the options passed have to say about it.
+        # This way, the user in the back has the last word about disabling cache and the NoCms::Pages engine
+        # control the cache over the NoCms::Block engine
+        options[:cache_enabled] = false if !NoCms::Pages.cache_enabled || !page.cache_enabled
         render_block block, options
       end
     end
