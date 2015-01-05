8a4ee9b9903ccde26f8ee5809512eafcf452dc9b
David J. Brenes <davidjbrenes@gmail.com>
2015-01-05 14:40:09 +0100

    Block decorator fixed (Syntax error) and included on the Block class

--- app/decorators/no_cms/blocks/block_decorator.rb
+++ app/decorators/no_cms/blocks/block_decorator.rb
@@ -5,7 +5,7 @@ NoCms::Blocks::Block.class_eval do
   has_and_belongs_to_many :pages, class_name: 'NoCms::Pages::Page'
 
   def set_default_position
-    self.update_attribute :position, ((pages.map{|p| p.blocks.pluck(:position)}).flatten.compact.max || 0) + 1) if self[:position].blank? && self.pages.exists?
+    self.update_attribute :position, (((pages.map{|p| p.blocks.pluck(:position)}).flatten.compact.max || 0) + 1) if self[:position].blank? && self.pages.exists?
   end
 
 end
--- lib/nocms/pages/engine.rb
+++ lib/nocms/pages/engine.rb
@@ -6,6 +6,12 @@ module NoCms
   module Pages
     class Engine < ::Rails::Engine
       isolate_namespace NoCms::Pages
+
+      config.to_prepare do
+        Dir.glob(NoCms::Pages::Engine.root + "app/decorators/**/*_decorator*.rb").each do |c|
+          require_dependency(c)
+        end
+      end
     end
   end
 end
