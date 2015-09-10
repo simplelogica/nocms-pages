ae96a22134dc5570f1ff0623a1c7006ece811f55
Luis Mendo <luis.mendo@the-cocktail.com>
2015-09-10 13:05:09 +0200

    Remove gem's migrations from dummy app.

--- spec/dummy/db/migrate/20140806074938_add_layout_to_no_cms_pages_page.no_cms_pages.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from no_cms_pages (originally 20140806074811)
-class AddLayoutToNoCmsPagesPage < ActiveRecord::Migration
-  def change
-    add_column :no_cms_pages_pages, :layout, :string
-  end
-end
